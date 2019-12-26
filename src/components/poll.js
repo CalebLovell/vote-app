a5aa73cce62a6f189f114a2d3063145fa7a4cf79
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-12-25 21:13:32 -0700

    add poll component and shortid generator

---
+++ src/components/poll.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const PollContainer = styled.div`
+	width: 600px;
+	height: 600px;
+	background-color: lightgrey;
+`;
+
+const Poll = () => {
+	const [pollType, setPollType] = useState(`single`);
+
+	return (
+		<PollContainer>
+			<h1>Create a poll below</h1>
+			<h2>New Poll</h2>
+			<input placeholder='New Poll'></input>
+			<h2>New Answers</h2>
+			<input placeholder='New Poll'></input>
+			<input placeholder='New Poll'></input>
+			<input placeholder='New Poll'></input>
+			<input placeholder='New Poll'></input>
+			<button>Create Voteable</button>
+		</PollContainer>
+	);
+};
+
+export default Poll;
